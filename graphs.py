7ee68c270ffbe0d30f8255ee13607f5763b65daa
Jude <judedelaney06@gmail.com>
2025-01-04 18:44:19 +0000

    added bar chart for salary buy education type and made sure it saved only the graphs html not a full page

--- graphs.py
+++ graphs.py
@@ -8,11 +8,16 @@ age = data["age_corilation"]["male"]["age"] +data["age_corilation"]["female"]["a
 salary = data["age_corilation"]["male"]["salary"] + data["age_corilation"]["female"]["salary"]
 gender = ["male"]*len(data["age_corilation"]["male"]["age"]) + ["female"]*len(data["age_corilation"]["female"]["age"])
 
-fig = px.scatter(x=age, y=salary,color = gender ,labels={'x':'Age', 'y':'Salary'}, title ="Age Salary Corrilation")
+fig = px.scatter(x=age, y=salary,color = gender ,labels={'x':'Age', 'y':'Salary'}, title ="Age Salary Corrilation",)
 
 fig.show()
-fig.write_html('agecorrilation.html')
+fig.write_html('agecorrilation.html', full_html= False)
 
 fig = px.pie(values=data["salary_gender"], names= ["Female pay", "Extra men pay men recieve"],title="Gender wage gap")
 fig.update_traces(textinfo="label+percent")
+fig.show() 
+fig.write_html('genderpaygap.html', full_html= False)
+
+
+fig = px.bar(x=["Bachlors","Masters","Phd"],y=data["education"]["median"],title="Median Salary by level of Education",color=["Bachlors","Masters","Phd"])
 fig.show()
