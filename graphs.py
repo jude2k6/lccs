ed6eeef1a07f881ef3883f16c013553c736ab30b
skc05 <judedelaney06@gmail.com>
2025-01-16 13:02:54 +0000

    chnaged graphs

--- graphs.py
+++ graphs.py
@@ -12,14 +12,16 @@ salary = data["age_corilation"]["male"]["salary"] + data["age_corilation"]["fema
 #generates a gender list based of the lenght of the male and female age lists this is to add colours
 gender = ["male"]*len(data["age_corilation"]["male"]["age"]) + ["female"]*len(data["age_corilation"]["female"]["age"])
 #creates a scatter plot based on ages and salarys and writes the html to a file in /website/graphs
-fig = px.scatter(x=age, y=salary,color = gender ,labels={'x':'Age', 'y':'Salary'}, title ="Age Salary Corrilation",)
-fig.show()
+fig = px.scatter(x=age, y=salary,color = gender ,
+                 labels={'x':'Age', 'y':'Salary',"color":"Gender"},
+                title ="Age Salary Corrilation",)
+
 fig.write_html(basepath+'/website/graphs/agegraph.html', full_html= False)
 
 
 fig = px.pie(values=data["salary_gender"], names= ["Female pay", "Extra men pay men recieve"],title="Gender wage gap")
 fig.update_traces(textinfo="label+percent")
-fig.show() 
+ 
 fig.write_html(basepath+'/website/graphs/gendergraph.html', full_html= False)
 
 
@@ -27,7 +29,7 @@ fig = px.bar(x=["Bachlors","Masters","Phd"],y=data["education"]["median"],
              title="Median Salary by level of Education",
              labels={'x':'Education', 'y':'Salary'},
              color=["Bachlors","Masters","Phd"])
-fig.show()
+
 fig.write_html(basepath+'/website/graphs/educationgraph.html', full_html= False)
 
 #top and bottom 10%
@@ -38,5 +40,5 @@ fig.write_html(basepath+'/website/graphs/educationgraph.html', full_html= False)
 fig = px.histogram( x=data["education"]["top_bottom"]["education"], y=data["education"]["top_bottom"]["salary"],
              color=data["education"]["top_bottom"]["type"], barmode='group',
              histfunc='avg',title='Top and bottom 10% by salary')
-fig.show()
+
 fig.write_html(basepath+'/website/graphs/top_bottomgraph.html', full_html= False)
